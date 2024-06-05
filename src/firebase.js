1a3a22c1ee649edcdf55d008d06848705c643a32
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2024-06-06 01:37:21 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2ouVGOTVofb6OFVYusnMCdaNkuv96sYU",
-  authDomain: "chitchat-b1b3c.firebaseapp.com",
-  projectId: "chitchat-b1b3c",
-  storageBucket: "chitchat-b1b3c.appspot.com",
-  messagingSenderId: "745180830192",
-  appId: "1:745180830192:web:56f87f8a9db8967340a535",
+  confidential 
 };
 
 export const app = initializeApp(firebaseConfig);
